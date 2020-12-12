6eb7670f3677c48c85896ee359d53a967606200f
Jayna Leitze <jayna.leitze@gmail.com>
2020-12-12 15:21:55 -0600

    create form to enter vet information

--- src/components/vet/VetForm.js
+++ src/components/vet/VetForm.js
@@ -1,2 +1,31 @@
 import React from "react"
 import {useForm} from useForm
+import VetContext from "./VetProvider"
+
+export const VetForm = () => {
+    const {addVet} = useContext(VetContext)
+    const {register, handleSubmit} = useForm()
+    const onSubmit = (data) => {
+        addVet(data)
+    }
+    return (
+        <form className="vetForm" onSubmit={handleSubmit(onSubmit)}>
+            <h3>Veterinarian</h3>
+            <label>Vet Name:</label>
+            <input type="text" name="vetName" ref={register} />
+            <label>Vet Address:</label>
+            <textarea type="text" name="addressLine1" ref={register}/>
+            <input type="text" name="addressLine2" ref={register}/>
+            <input type="text" name="city" ref={register}/>
+            <input type="text" name="state" ref={register}/>
+            <input type="text" name="zip" ref={register}/>
+            <label>Vet Phone Number:</label>
+            <input type="text" ref={register}/>
+            <label>Vet Email:</label>
+            <input type="text" ref={register}/>
+            <label>Website:</label>
+            <input type="text" name="website" ref={register}/>
+
+        </form>
+    )
+}
