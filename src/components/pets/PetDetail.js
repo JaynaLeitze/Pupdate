f6a89c737c3551d40d5a44f5ef6ae4015cdab172
Jayna Leitze <jayna.leitze@gmail.com>
2020-12-13 19:08:03 -0600

    add borders in pet.css

--- src/components/pets/PetDetail.js
+++ src/components/pets/PetDetail.js
@@ -25,19 +25,30 @@ export const PetDetails = (props) => {
   }, [pets]);
   console.log(vetRecords);
   return (
-    <section className="petCard">
-      <img width="300px" src={pet.image} />
-      <h3 className="petName">{pet.petName}</h3>
-      <div className="petBreed">Breed: {pet.petBreed}</div>
-      <div className="petAge">Age:{pet.petAge}</div>
-      <div className="petGender">
-        Gender:{pet.gender === true ? "Male" : "Female"}
-      </div>
-      <div className="petWeight">Weight:{pet.petWeight}</div>
-      <div className="petConditions">
-        Chronic Conditions:{pet.petConditions}
-      </div>
-      <div className="petMedications">Medications: </div>
-    </section>
+    <article className="petDashboard">
+      <section className="petCard">
+        <img width="300px" src={pet.image} />
+        <h3 className="petName">{pet.petName}</h3>
+        <div className="petBreed">Breed: {pet.petBreed}</div>
+        <div className="petAge">Age:{pet.petAge}</div>
+        <div className="petGender">
+          Gender:{pet.gender === true ? "Male" : "Female"}
+        </div>
+        <div className="petWeight">Weight:{pet.petWeight}</div>
+        <div className="petConditions">
+          Chronic Conditions:{pet.petConditions}
+        </div>
+        <div className="petMedications">Medications: </div>
+      </section>
+      <section className="symptoms">
+        <h3>Symptoms</h3>
+      </section>
+      <section className="medRecords">
+        <h3>Medical Records</h3>
+      </section>
+      <section className="vetInfo">
+        <h3>Veterinarian</h3>
+      </section>
+    </article>
   );
 };
