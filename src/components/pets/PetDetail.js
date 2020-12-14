e27c557f4bacf37f132525eb2b1b71d485d2a064
Jayna Leitze <jayna.leitze@gmail.com>
2020-12-13 18:48:13 -0600

    did lots of things in VetList and PetDetails

--- src/components/pets/PetDetail.js
+++ src/components/pets/PetDetail.js
@@ -3,27 +3,35 @@ import { PetContext } from "./PetProvider";
 import "./Pet.css";
 
 export const PetDetails = (props) => {
-  const { pets, addPets, getPets, getPetById } = useContext(PetContext);
+  const {
+    pets,
+    addPets,
+    getPets,
+    getPetById,
+    getRecordByPetId,
+    vetRecords,
+  } = useContext(PetContext);
   const petId = parseInt(props.match.params.petId);
   const [pet, setPet] = useState({});
 
   useEffect(() => {
     // getPetById(petId).then(setPet);
-    getPets();
+    getPets().then(() => getRecordByPetId(petId));
   }, []);
   useEffect(() => {
     console.log(props);
     const thisPet = pets.find((pet) => pet.id === petId) || {};
     setPet(thisPet);
   }, [pets]);
+  console.log(vetRecords);
   return (
-    <section className="pet">
+    <section className="petCard">
       <img width="300px" src={pet.image} />
       <h3 className="petName">{pet.petName}</h3>
       <div className="petBreed">Breed: {pet.petBreed}</div>
       <div className="petAge">Age:{pet.petAge}</div>
       <div className="petGender">
-        Gender:{pet.gender === true ? Male : Female}
+        Gender:{pet.gender === true ? "Male" : "Female"}
       </div>
       <div className="petWeight">Weight:{pet.petWeight}</div>
       <div className="petConditions">
