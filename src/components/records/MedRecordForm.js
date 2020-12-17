580076121550e02b848051a9af015e8993dfc58d
Jayna Leitze <jayna.leitze@gmail.com>
2020-12-16 18:03:38 -0600

    made changes

--- src/components/records/MedRecordForm.js
+++ src/components/records/MedRecordForm.js
@@ -7,27 +7,20 @@ import { PetContext } from "../pets/PetProvider";
 export const RecordForm = (props) => {
   const { register, handleSubmit } = useForm();
   const { addRecord } = useContext(RecordContext);
-  const { vets, getVets, getVetById } = useContext(VetContext);
-  const { pets, getPets } = useContext(PetContext);
-  const [vet, setVet] = useState({});
-  const [pet, setPet] = useState({});
-
-  // const vetId = vets.find((v) => v.id === vetId);
-  // const vetId = parseInt(props.match.params.vetId);
+  // const { vets, getVets, getVetById } = useContext(VetContext);
+  // const { pets, getPets, getRecordByPetId } = useContext(PetContext);
+  // const [vet, setVet] = useState({});
+  // const [pet, setPet] = useState({});
 
   const petId = parseInt(props.match.params.petId);
   console.log(petId);
   console.log(props);
 
   const onSubmit = (data) => {
-    console.log(props);
-    // data.vetId = vetId;
     data.petId = petId;
     addRecord(data).then(props.history.push(`/pets/${petId}`));
   };
-  useEffect(() => {
-    getPets().then(getVets);
-  }, []);
+
   return (
     <form className="recordForm" onSubmit={handleSubmit(onSubmit)}>
       <h3>Vet Record</h3>
@@ -41,5 +34,8 @@ export const RecordForm = (props) => {
       <input name="vaccinations" ref={register} />
       <input type="submit" ref={register} />
     </form>
+
+    //vet conditional: if vet exists, map through vetRecords to grab vet Id.
+    //if vet does not exist, present add vet button
   );
 };
