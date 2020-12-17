103424fddead14ec34aa22c471ea4a6ee24c23c3
Jayna Leitze <jayna.leitze@gmail.com>
2020-12-17 15:49:38 -0600

    clean up database

--- src/components/pets/Pet.js
+++ src/components/pets/Pet.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 //JSX for User's Pet List
 export const Pet = ({ pet }) => (
-  <section className="pet">
-    <div>
+  <section className="userPets">
+    <div className="userPetCard">
       <Link to={`/pets/${pet.id}`}>
         <img src={pet.image} width={300} />
       </Link>
+      <h4>{pet.petName}</h4>
     </div>
-    <h4>{pet.petName}</h4>
   </section>
 );
